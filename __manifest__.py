6e4b9018cc3edbdde2a74341aa6f235ffc5ac20c
ku7ush <ku7ush@gmail.com>
2021-09-21 18:00:52 +0200

    Module skel

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': '',
+    'description': 'Alla Scoperta delle Biodiversità',
+    'version': '1.0',
+    'author': 'Ciro Valerio Bonfrate',
+    'category': 'Website',
+
+    'depends': ['website'],
+    'data': [
+    	'views/assets.xml',
+    ],
+    'application': False,
+}
